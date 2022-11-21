a021b6b90d069d508e9c9e32b1262104c186e809
Pusha D <dmitry@khloptsov.ru>
2022-11-21 18:49:00 +0300

    Выполнены все задания спринта

---
+++ ImageFeed/ImageList/ImageListCell.swift
@@ -0,0 +1,20 @@
+//
+//  ImageListCell.swift
+//  ImageFeed
+//
+//  Created by DMITRY KHLOPTSOV on 18.11.2022.
+//
+
+import UIKit
+
+final class ImageListCell: UITableViewCell {
+    static let reuseIdentifier = "ImageListCell"
+    
+    @IBOutlet var cellImageView: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateView: UIView!
+    @IBOutlet var dateLabel: UILabel!
+
+
+    
+}
