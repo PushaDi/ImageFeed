96e8d76a8c3c5291fda617de9032563849393f99
Pusha D <dmitry@khloptsov.ru>
2022-12-14 18:31:32 +0300

    Добавлен TabBar, NavigationBar, добавлен переход на полномасштабную картинку по умолчанию

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by DMITRY KHLOPTSOV on 06.12.2022.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var profileImageView: UIImageView!
+    @IBOutlet private var exitButton: UIButton!
+    @IBOutlet private var profileNameLabel: UILabel!
+    @IBOutlet private var profileIDLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+}
---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by DMITRY KHLOPTSOV on 12.12.2022.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet private var imageView: UIImageView!
+}
